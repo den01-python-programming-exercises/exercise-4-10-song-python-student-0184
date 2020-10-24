36a48623d736c743056584be391e2b921943c64d
pythonstudent01 <python-student@den01.co.uk>
2020-10-24 12:26:44 +0000

    Attempt solutuon

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
+    garden = Song("In The Garden", 10910)
+    print("The song " + garden.name + " has a length of " + str(garden.length) + " seconds.")
 
 if __name__ == '__main__':
+    from song import Song
     main()
---
+++ src/song.py
@@ -0,0 +1,12 @@
+class Song:
+
+  def __init__(self, name, length):
+    self.name = name
+    self.length = length
+
+  def name(self):
+    return self.name
+
+  def length(self):
+    return self.length
+
